300f0e1beef61f2a516fe65bf391a27a91b115ab
Darya201 <daryad39@gmail.com>
2025-07-15 18:06:42 +0300

    Add files via upload

---
+++ lesson_2_task_5.py
@@ -0,0 +1,17 @@
+def month_to_season(month):
+    if 9 <= month <= 11:
+        return "Осень"
+    elif 6 <= month <= 8:
+        return "Лето"
+    elif 3 <= month <= 5:
+        return "Весна"
+    elif month in (12, 1, 2):
+        return "Зима"
+    else:
+        return "Неверный номер месяца"
+
+try:
+    month = int(input("Введите номер месяца (1-12): "))
+    print(month_to_season(month)) 
+except ValueError:
+    print("Пожалуйста, введите целое число от 1 до 12.")
